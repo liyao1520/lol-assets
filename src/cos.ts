6eb740126c5990f30b65af8e9136625a7631c4ec
李耀 <liyao@uzoo.cn>
2024-08-22 10:22:21 +0800

    fix: env var

--- src/cos.ts
+++ src/cos.ts
@@ -4,6 +4,10 @@ const cos = new COS({
   SecretId: process.env.TENCENTCLOUD_SECRET_ID,
   SecretKey: process.env.TENCENTCLOUD_SECRET_KEY
 })
+
+console.log(process.env.TENCENTCLOUD_SECRET_ID?.length)
+console.log(process.env.TENCENTCLOUD_SECRET_KEY?.length)
+
 const Region = 'ap-beijing'
 const Bucket = 'lol-1259483240'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
