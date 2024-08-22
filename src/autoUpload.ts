ea6303e498b76381b7e15e6bece02d73c967e9b9
李耀 <liyao@uzoo.cn>
2024-08-22 10:32:33 +0800

    update

--- src/autoUpload.ts
+++ src/autoUpload.ts
@@ -5,8 +5,9 @@ import { fileURLToPath } from 'node:url'
 import { dirname, resolve } from 'node:path'
 import pRetry from 'p-retry'
 
-import { getObject } from './cos'
+import { getObject, putObject } from './cos'
 import { ensureFile, writeFile, remove, readdir } from 'fs-extra'
+import { readFile } from 'node:fs/promises'
 
 const __dirname = dirname(fileURLToPath(new URL(import.meta.url)))
 const __data__ = resolve(__dirname, '../__data__')
@@ -20,24 +21,22 @@ const putFile = async (key: string, data: unknown) => {
 
 async function main() {
   await remove(__data__)
-
+  let listFile: string = ''
+  try {
+    listFile = await readFile(resolve(__data__, 'champion/list.json'), 'utf-8')
+    console.log('listFile has')
+  } catch (e) {
+    console.log(e)
+  }
   const startTime = Date.now()
   const response = await pRetry(getChampions, { retries: 4 })
   const { championRankingList, metaData } = response
-  try {
-    // 检测 资源是否 更新
-    const obj = await getObject('champion/list.json')
-
-    const res = JSON.parse(obj.Body.toString())
-
+  if (listFile) {
+    const res = JSON.parse(listFile)
     if (res.metaData.cached_at === metaData.cached_at) {
       console.log('list.json is up to date')
       return
     }
-  } catch (e) {
-    console.log(e)
-    if (e.statusCode === 404) return
-
   }
 
   await putFile('champion/list.json', response)
@@ -55,14 +54,6 @@ async function main() {
     console.log(`putFile ${key}.json done`)
   }
 
-  // 写入文件
-  const files = await readdir(resolve(__data__, 'champion'))
-  for (const file of files) {
-    const key = file.replace('.json', '')
-    const obj = await getObject(`champion/${key}.json`)
-    await pRetry(() => putFile(`champion/${key}.json`, JSON.parse(obj.Body.toString())), { retries: 2 })
-    console.log(`putFile ${key}.json done`)
-  }
   console.log('finish')
   console.log(`all done, cost ${(Date.now() - startTime) / 1000}s`)
 }
